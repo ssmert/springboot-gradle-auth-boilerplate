e406d0be0a746974bf4c8ead6cc2dc47857a71b1
전종현 <ssmert@naver.com>
2020-07-17 16:10:09 +0900

    IMP: 등록,수정 시 자동으로 일시,변경자 할당하도록 수정함

--- src/main/java/com/example/demo/core/infrastructure/configuration/Configurations.java
+++ src/main/java/com/example/demo/core/infrastructure/configuration/Configurations.java
@@ -2,6 +2,7 @@ package com.example.demo.core.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 
@@ -13,6 +14,7 @@ import java.nio.charset.StandardCharsets;
  * @author jonghyeon
  */
 @Configuration
+@EnableJpaAuditing
 public class Configurations {
     @Bean
     public HttpMessageConverter<String> responseBodyConverter() {
